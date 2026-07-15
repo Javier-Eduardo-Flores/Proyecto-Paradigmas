dcfb2142ece17e734d75f21d34a3a48a47eefd2c
Carlos Machigua <carlos.machigua@unah.hn>
2026-07-15 04:02:03 +0000

    Anadidos los modulos que hizo el ingeniero para manipular el DOM

---
+++ public/assets/js/modules/dom-basic-html.mjs
@@ -0,0 +1,21 @@
+/**
+ * DOM: Document Object Model
+ * @author jose.inestroza@unah.edu.hn
+ * @version 0.1.1
+ * @date 26/06/29
+ * @since 26/06/25
+ * 
+ */
+const tag = (name, content = `${placeholder(name)} content ${name}`, style ="") => {
+    return `<${name} ${style}> ${content} </${name}>`;
+};
+
+const h = (content) => tag("h1", content);
+const p = (content) => tag("p", content);
+const b = (content) => tag("strong", content);
+
+export{
+    h,
+    p,
+    b
+}
