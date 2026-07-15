13d7a989db7d093ac7a4ec9011868007b429ed6e
Javier-Eduardo-Flores <javierfloresv30@gmail.com>
2026-07-14 18:32:04 -0600

    Primer commit con datos en la base de conocimientos

---
+++ data-model/db.pl
@@ -0,0 +1,78 @@
+estudiante('Javier Eduardo Flores Valladares','20221002840','flores.javier@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Jose Eduardo Suarez Valdez','20211003860','jose.valdez@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Maria Jose Castillo Lopez','20211001850','maria.castillo@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Mario Josue Cruz Ochoa','20231001441','mario.cruz@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Josue Manuel Espinal Flores','20241004845','josue.espinal@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Kevin Omar Morales Pineda','20231002241','kevin.morales@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Brayan David Perdomo Salgado','20211002341','brayan.salgado@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Claudia Patricia Morales Zelaya','20221002243','claudia.zelaya@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Mercedes Paola Lagos Caceres','20241002821','mercedes.lagos@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Jeremy Alfredo Jimenes Alfaro','20231001834','alfredo.alfaro@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Alejandro Manuel Flores Suazo','20201002174','alejandro.suazo@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Mauricio Joel Perez Dominguez','20271007883','mauricio.dominguez@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Allan Daniel Mendez Mendoza','20251002314','allan.mendez@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Victor Fernando Mejia Ramos','20241002864','victor.mejia@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Daniela Elizabeth Osorio Caceres','20261002647','daniela.osorio@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Lucia Fernanda Mejia Perdomo','20261002149','lucia.mejia@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Brenda Mariel','20201002341','brenda.mariel@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Samuel Edgardo Matamoros Paz','20231004841','samuel.matamoros@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Pedro Fabian Gonzales Porro','20261001407','pedro.porro@unah.hn','Ingenieria en Sistemas Computacionales').
+estudiante('Vanessa Yamileth Toro Coello','20251002560','vanessa.toro@unah.hn','Ingenieria en Sistemas Computacionales').
+
+profesor('Carlos Jose Estrada Matamoro','0801199801452','carlos.estrada@unah.edu.hn').
+profesor('Sofia Martinez Lopez','0501200002341','sofia.martinez@unah.edu.hn').
+profesor('Fernando Alonso Gomez Cruz','0101199900892','fernando.gomez@unah.edu.hn').
+profesor('Ricardo Andres Morales Mendoza','0801199703415','ricardo.morales@unah.edu.hn').
+profesor('Jorge Luis Reyes Padilla','1804200101123','jorge.reyes@unah.edu.hn').
+profesor('Ana Manuela Castro Rivera','0801200004567','ana.castro.r@unah.edu.hn').
+profesor('Marcos Omar Morales Pineda','0601199801890','omar.morales@unah.edu.hn').
+profesor('Franklin Joel Dubon Caballero','0801200202104','frankin.caballero@unah.edu.hn').
+profesor('Axel Roberto Zelaya Ortiz','1501199605678','axel.ortiz@unah.edu.hn').
+profesor('Sonia Raquel Morales Calderon','0501199903891','sonia.calderon@unah.edu.hn').
+profesor('Jairo Alexander Sierra Andrade','0801200102903','jairo.sierra@unah.edu.hn').
+profesor('Erika Yaneth Benítez Osorio','0301199804125','erika.benitez@unah.edu.hn').
+
+
+/*
+Requisitos (codigoClase,Nombre de la asignatura,UV)
+*/
+
+asignatura('ISC-101','Introduccion a la Ingenieria en Sistemas Computacionales',4).
+asignatura('ISC-102','Programacion Estructura',4).
+asignatura('ISC-103','Programacion Orientada a Objetos',4).
+asignatura('ISC-204','Paradigmas de Programacion',4).
+asignatura('ISC-211','Estructuras de Datos',4).
+asignatura('ISC-321','Fundamentos de Bases de Datos',5).
+asignatura('ISC-351','Contabilidad Financiera',4).
+asignatura('ISC-331','Redes de Datos I',4).
+asignatura('ISC-333','Sistemas Operativos I',4).
+asignatura('ISC-312','Teoria de la Computacion',4).
+asignatura('ISC-341','Sistemas de Informacion',4).
+asignatura('ISC-332','Redes de Datos II',4).
+asignatura('ISC-334','Sistemas Operativos II',4).
+asignatura('ISC-305','Programacion Web',4).
+asignatura('ISC-313','Compiladores',4).
+asignatura('ISC-306','Analisis de Requerimientos',4).
+asignatura('ISC-336','Diseño Digital',4).
+asignatura('ISC-407','Programacion Movil',5).
+asignatura('ISC-414','Inteligencia Artificial',4).
+asignatura('ISC-435','Administracion de Servidores',4).
+asignatura('ISC-437','Arquitectura de Computadoras',4).
+asignatura('ISC-408','Ingenieria de Software',4).
+asignatura('ISC-422','Administracion de Bases de Datos',4).
+asignatura('ISC-442','Seguridad Informatica',4).
+asignatura('ISC-443','Industria de TI',4).
+asignatura('ISC-409','Calidad de Software',4).
+asignatura('ISC-423','Ciencia de Datos',4).
+asignatura('ISC-415','Tecnologias Emergentes',4).
+asignatura('ISC-445','Proyectos de TI',4).
+asignatura('ISC-552','Seminario de Investigacion',4).
+asignatura('ISC-544','Auditoria Informatica',4).
+asignatura('ISC-546','Ejecucion de Proyectos de TI',4).
+
+/*
+Requisitos (Clase,requisito1,requisito2)
+*/
+
+requisitos('ISC-204','ISC-103').
+requisitos('ISC-306','ISC-341','ISC-321').
