d25c2e6810e4a8bf530bfb9fa2ed90effa6900d2
Carlos Machigua <carlos.machigua@unah.hn>
2026-07-25 07:12:35 +0000

    Anade la funcionalidad de hacer click en un alumno para ver su historial, hacer click en un profesor para ver su historial de alumnos. Falta el de ver las notas mas altas por asignatura.

---
+++ public/assets/api/PHP/topGlobalesAsignaturas.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * @author jose.inestroza@unah.edu.hn
+ * @author Machigua
+ * @version 0.1.1
+ * @date 26/06/29
+ * @since 26/06/25
+ * 
+ * 
+Endpoint encargado de consultar las clases cursadas de cierto estudiante a Prolog, valiendose del servicio
+Sub-Consultas
+
+*/ 
+
+header("Content-Type: application/json");
+
+["resultadoConsulta" => $consultaProlog] = include __DIR__."/../../../../service/PHP/Sub-Consultas/index.php";
+
+$dni = $_GET["DNI"];
+
+
+$consulta = "swipl --quiet -g \"consult('%s'), consult('%s'), (forall(profesor_estudiante('{$dni}',NombreEstudiante,CuentaEstudiante,CorreoEstudiante), format('~w,~w,~w~n', [NombreEstudiante, CuentaEstudiante, CorreoEstudiante])) ; true)\" -t halt  | python3 %s Estudiantes Nombre,Cuenta,Correo";
+
+
+$json = $consultaProlog($consulta);
+
+echo($json);
+
